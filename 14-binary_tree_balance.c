c343b3cdd9e56a30fddc14c9f68eeae6d5aedff8
dagmawi-demeke <67114822+dagmawi-demeke@users.noreply.github.com>
2022-11-17 16:18:06 +0300

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,39 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - A function that measures the balance factor of a binary tree.
- * @tree: Pointer to root node to calculate balance factor.
- * Return: The balance factor of the tree.
+ * binary_tree_rotate_left - Left-rotates a binary tree.
+ * @tree: A pointer to the root node of the tree to rotate.
+ *
+ * Return: A pointer to the new root node after rotation.
  */
-int binary_tree_balance(const binary_tree_t *tree)
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	binary_tree_t *pivot, *tmp;
 
-	if (!tree)
-		return (0);
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return (left_height - right_height);
-}
+	pivot = tree->right;
+	tmp = pivot->left;
+	pivot->left = tree;
+	tree->right = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
+	}
 
-/**
- * binary_tree_height - gets the height of a binary tree
- * @tree: root node to draw height from for tree
- * Return: size_t representing height, 0 on failure or NULL.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_size = 0;
-	size_t right_size = 0;
-
-	if (!tree)
-		return (0);
-
-	left_size += 1 + binary_tree_height(tree->left);
-	right_size += 1 + binary_tree_height(tree->right);
-	if (left_size > right_size)
-		return (left_size);
-
-	return (right_size);
+	return (pivot);
 }
